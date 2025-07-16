19a9b9fac01edcf857a66efde556cb8984a33961
Raja Sandhu <rajasandhu@imacpro.lan>
2025-07-15 22:06:45 -0400

    Add safelist to support custom Tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
+  safelist: [
+    'from-[#141414]',
+    'to-[#292929]',
+    'bg-gray-800/30'
+  ],
   theme: {
     extend: {}
   },
   plugins: []
-};
-
+}
