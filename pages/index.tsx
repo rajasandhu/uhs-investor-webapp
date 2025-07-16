389097ec6a48b26493a8364cb2cd51e44ecfc81c
Raja Sandhu <rajasandhu@imacpro.lan>
2025-07-15 22:25:12 -0400

    Safely inject Tailwind class tokens to prevent purge

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,6 +80,7 @@ export default function Home() {
               </div>
             ))}
             <div ref={bottomRef} />
+              <div className=\"hidden from-[#141414] to-[#292929] bg-gray-800/30\"></div>
 <div className="hidden from-[#141414] to-[#292929] bg-gray-800/30"></div>
 
 
